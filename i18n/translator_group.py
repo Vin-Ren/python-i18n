4d45815442d83b35f7417a1cfb5cb8700326e8ec
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2022-12-15 22:27:31 +0900

    TranslatorGroup without namespace can be created.

--- i18n/translator_group.py
+++ i18n/translator_group.py
@@ -6,7 +6,7 @@ from .translator import t
 class TranslatorGroup:
     TRANSLATION_FORMAT='{namespace}.{key}' # possible keys = namespace, locale, key
     
-    def __init__(self, namespace, locale=None):
+    def __init__(self, namespace='', locale=None):
         self.namespace=namespace
         self.locale=locale if locale is not None else get('locale')
         self.data={'namespace':self.namespace, 'locale':self.locale}
@@ -16,14 +16,14 @@ class TranslatorGroup:
     
     def __call__(self, key, *args, **kwds):
         [kwds.__setitem__(k,v) for k,v in self.data.items() if k not in kwds]
-        return t(self.TRANSLATION_FORMAT.format(**self.data, key=key), *args, **kwds)
+        return t(self.TRANSLATION_FORMAT.format(**self.data, key=key).lstrip('.'), *args, **kwds)
     
     def __getitem__(self, key):
-        return t(self.TRANSLATION_FORMAT.format(**self.data, key=key), **self.data)
+        return t(self.TRANSLATION_FORMAT.format(**self.data, key=key).lstrip('.'), **self.data)
     
     def reset(self, all_locale=False):
         """Resets all of the accessible translations of this TranslatorGroup."""
-        accessible_prefix=self.TRANSLATION_FORMAT.format(**self.data, key='')
+        accessible_prefix=self.TRANSLATION_FORMAT.format(**self.data, key='').lstrip('.')
         
         for locale in translations_container:
             if (locale==self.locale) or all_locale:
